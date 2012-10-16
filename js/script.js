60fbccc3759561cd0e2b13c28dd74e24edb0bb88
Mohit Athwani <mohit@geeksincorporated.net>
2012-10-17 02:41:21 +0530

    Timer Fixed to 20 days since 17 October 2012

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
 $(function(){
 	
-	var note = $('#note'),
-		ts = new Date(2012, 0, 1),
-		newYear = true;
-	
-	if((new Date()) > ts){
-		// The new year is here! Count towards something else.
-		// Notice the *1000 at the end - time must be in milliseconds
-		ts = (new Date()).getTime() + 20*24*60*60*1000;
-		newYear = false;
-	}
-		
+	var note = $('#note');
+	var ts = (new Date(2012, 9, 17)).getTime() + 20*24*60*60*1000;
+
 	$('#countdown').countdown({
 		timestamp	: ts,
 		callback	: function(days, hours, minutes, seconds){
 			
 			var message = "";
-			
-			//message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-			//message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-			//message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-			//message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-			
-			if(newYear){
-				message += "left until the new year!";
-			}
-			else {
+		
 				message += "Our designer(s) and developer(s) are working their a**es off ! We hope to bring you a limited beta verion of TeraMO.in before the countdown ends. Cheers !";
-			}
 			
 			note.html(message);
 		}
